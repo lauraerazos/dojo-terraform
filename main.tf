9f61f50058ac8607cd0392e91dbe3392d21109a7
lauraerazos <lauraerazo636@gmail.com>
2020-03-28 12:14:40 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+	region = "us-east-2"
+}
+
+
+resource "aws_instance" "example" {
+	ami = "ami-0e01ce4ee18447327"
+	instance_type = "t2.micro"
+
+    tags = {
+        Name = "terraform-lauraerazo"
+    }
+}
